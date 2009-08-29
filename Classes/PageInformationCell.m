f53906d60a020e2f1928e5e7e9e59ecc22a6ed23
KISHIKAWA Katsumi <kishikawakatsumi@mac.com>
2009-08-30 01:51:18 +0900

    Bookmark list cell refined.

--- Classes/PageInformationCell.m
+++ Classes/PageInformationCell.m
@@ -45,14 +45,14 @@
 	if ([commentText length] > 0) {
 		[[UIColor blackColor] set];
 		[commentText drawInRect:CGRectMake(20.0f, 1.0f, 280.0f, 74.0f) withFont:[UIFont systemFontOfSize:12.0f] lineBreakMode:UILineBreakModeTailTruncation];
-		[numberText drawInRect:CGRectMake(0.0f, 35.0f, 16.0f, 21.0f) withFont:[UIFont systemFontOfSize:9.0f] lineBreakMode:UILineBreakModeClip alignment:UITextAlignmentRight];
+		[numberText drawInRect:CGRectMake(0.0f, 38.0f, 16.0f, 21.0f) withFont:[UIFont systemFontOfSize:9.0f] lineBreakMode:UILineBreakModeClip alignment:UITextAlignmentRight];
 		[[UIColor grayColor] set];
 		[userText drawInRect:CGRectMake(20.0f, 69.0f, 280.0f, 20.0f) withFont:[UIFont systemFontOfSize:12.0f] lineBreakMode:UILineBreakModeTailTruncation alignment:UITextAlignmentRight];
 	} else {
 		[[UIColor blackColor] set];
-		[numberText drawInRect:CGRectMake(0.0, 1.0, 16.0, 18.0) withFont:[UIFont systemFontOfSize:9.0f] lineBreakMode:UILineBreakModeClip alignment:UITextAlignmentRight];
+		[numberText drawInRect:CGRectMake(0.0f, 4.0f, 16.0f, 18.0f) withFont:[UIFont systemFontOfSize:9.0f] lineBreakMode:UILineBreakModeClip alignment:UITextAlignmentRight];
 		[[UIColor grayColor] set];
-		[userText drawInRect:CGRectMake(20.0, 1.0, 280.0f, 18.0f) withFont:[UIFont systemFontOfSize:12.0f] lineBreakMode:UILineBreakModeTailTruncation alignment:UITextAlignmentRight];
+		[userText drawInRect:CGRectMake(20.0f, 1.0f, 280.0f, 18.0f) withFont:[UIFont systemFontOfSize:12.0f] lineBreakMode:UILineBreakModeTailTruncation alignment:UITextAlignmentRight];
 	}
 }
 
